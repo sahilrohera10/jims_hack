7f10a4b49fb1c231c74a23f72198666a88276d4e
Sahil Arora <sahilaroraji2002@gmail.com>
2024-02-06 06:31:13 +0530

    adding screens

--- app_native/App.js
+++ app_native/App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Pay3 from './src/screens/pay3';
+import SuccessfulScreen from './src/screens/sucessfullScreen';
+import AnalyticsScreen from './src/screens/analyticsScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text>hello</Text> */}
       <StatusBar style="auto" />
+      {/* <Pay3 /> */}
+          {/* <SuccessfulScreen /> */}
+          <AnalyticsScreen />
     </View>
   );
 }
